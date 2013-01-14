9c1a072cb67659bff0f22ce645db6096085ac673
Samrat Man Singh <samratmansingh@gmail.com>
2013-01-14 11:01:38 +0545

    Use clostache instead of laser
    --HG--
    branch : clostache

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [me.raynes/laser "0.1.14"]
+                 [de.ubercode.clostache/clostache "1.3.1"]
                  [fs "1.3.3"]
                  [clj-time "0.4.4"]
                  [slugger "1.0.1"]
