d13d0e9d548840f0c8006d36d070af3d6ba1ed1f
Samrat Man Singh <samratmansingh@gmail.com>
2013-06-05 18:02:27 +0545

    Bump clojure to 1.5.1

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://samrat.me/ecstatic"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [hiccup "1.0.2"]
                  [fs "1.3.3"]
                  [clj-time "0.4.4"]
