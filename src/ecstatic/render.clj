abb8ed5794fb6d508683bc85b5e7626d9f1be423
Karl Heinrichmeyer <karl.heinrichmeyer@gmail.com>
2013-10-10 19:01:31 +0200

    Add c as alias for ecstatic.code to ecstatic.render. This is shorter.

--- src/ecstatic/render.clj
+++ src/ecstatic/render.clj
@@ -7,6 +7,7 @@ The namespace also provides shortcuts for hiccup namespaces"
             [hiccup.form :as hform]
             [hiccup.util :as hutil]
             [hiccup.page :as hpage]
-            [ecstatic.code :as code])
+            [ecstatic.code :as code]
+            [ecstatic.code :as c])
   (:use [ecstatic.api]
         [ecstatic.core :only  [*metadata* *content*]]))
