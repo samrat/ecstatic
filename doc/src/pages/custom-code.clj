ec84340de7e0fb2e9b96ab6ea23c9b3272d58148
Karl Heinrichmeyer <karl.heinrichmeyer@gmail.com>
2013-10-10 19:09:52 +0200

    Add a little content to the doc-page about custom-code.

--- doc/src/pages/custom-code.clj
+++ doc/src/pages/custom-code.clj
@@ -2,4 +2,9 @@
 Title: Custom-Code
 template: page
 ---
-"custom code will be here soon."
+[:div
+ (c/row
+  (c/col "col-12"
+         [:h1 "Custom Code"]
+         "Custom code lets you write your own helper functions for situations that
+need more power than snippets or that would leed to codebloat if you would write plain html or hiccup. Remember: DRY (Don't repeat yourself)"))]
