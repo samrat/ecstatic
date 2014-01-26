319a1fea80ef3cdecf7e6cc1c4136f0e45b14352
Samrat Man Singh <samratmansingh@gmail.com>
2014-01-26 16:10:56 +0545

    Add in-dir atom to avoid passing it everywhere.

--- src/ecstatic/core.clj
+++ src/ecstatic/core.clj
@@ -21,7 +21,7 @@
                                    (str (timbre/color-str
                                          :red
                                          (name (:level log))))))
-
+(def in-dir (atom nil))
 (def ^:dynamic *in-dir* nil)
 
 (defn file-metadata
@@ -62,16 +62,16 @@
 
 (defn all-pages
   "List of maps containing post-info."
-  [in-dir]
+  []
   (map (fn [file]
          (-> (assoc (file-metadata file) :file file)
              ;;(assoc :content (file-content file))
              (assoc :url (page-url file))))
-       (page-files in-dir)))
+       (page-files @in-dir)))
 
 (defn all-posts
   "List of maps containing post-info."
-  [in-dir]
+  []
   (->> (map (fn [file]
               (-> (assoc (file-metadata file) :file file)
                   (assoc :content (file-content file))
@@ -79,7 +79,7 @@
                   (assoc :human-readable-date (unparse
                                                (formatter "dd MMMMM, YYYY")
                                                (parse (:date (file-metadata file)))))))
-            (post-files in-dir))
+            (post-files @in-dir))
        (sort-by :date)
        (reverse)))
 
@@ -97,8 +97,8 @@
 (defn tag-buckets
   "Categorizes posts under tags. Returns {tag1 [post1 post2], tag2
    [post1]}"
-  [in-dir]
-  (->> (all-posts in-dir)
+  []
+  (->> (all-posts)
        (reduce (fn [m post]
                  (concat m (interleave (:tags post)
                                        (repeat (vector post)))))
@@ -115,59 +115,58 @@
        (apply concat)
        (set)))
 
-(defn render-hiccup [in-dir hiccup-data]
+(defn render-hiccup [hiccup-data]
   (binding [*ns* (the-ns 'ecstatic.render)
-            *in-dir* in-dir]
+            *in-dir* @in-dir]
     (html (eval hiccup-data))))
 
-(defmulti split-and-to-html (fn [in-dir file] (file-type file)))
+(defmulti split-and-to-html (fn [file] (file-type file)))
 
-(defmethod split-and-to-html :markdown [in-dir file]
+(defmethod split-and-to-html :markdown [file]
   (md/to-html (file-content file) [:fenced-code-blocks]))
 
-(defmethod split-and-to-html :clojure [in-dir file]
-  (render-hiccup in-dir (read-string (file-content file))))
+(defmethod split-and-to-html :clojure [file]
+  (render-hiccup (read-string (file-content file))))
 
-(defn snippet [in-dir name]
+(defn snippet [name]
   "Expects the name of a snippet and returns the corresponding html."
-  (let [file (snippet-files in-dir name)]
+  (let [file (snippet-files @in-dir name)]
     (cond
      (markdown-file? file) (md/to-html (slurp file))
-     (clojure-file? file) (render-hiccup in-dir (read-template (.getPath file)))))) ; TODO refactor call
+     (clojure-file? file) (render-hiccup (read-template (.getPath file)))))) ; TODO refactor call
 
 (def ^:dynamic *content* nil)
 (def ^:dynamic *metadata* nil)
 
 (defn render-template-content
   "Render the hiccup template 'template-content' without wrapping it with the doctype."
-  [in-dir template-content cont meta]
+  [template-content cont meta]
   (binding [*content* cont
             *metadata*  meta]
-    (render-hiccup in-dir template-content)))
+    (render-hiccup template-content)))
 
 (defn render-template-partially
   "Render the template with the name 'template-name' without wrapping it with
 the doctype."
-  [in-dir template-name cont meta]
-  (let [template (read-template (str in-dir "/templates/" template-name ".clj"))]
-    (render-template-content in-dir template cont meta)))
+  [template-name cont meta]
+  (let [template (read-template (str @in-dir "/templates/" template-name ".clj"))]
+    (render-template-content template cont meta)))
 
 (defn render-template
   "Fully render the template 'template' to a html5 page."
-  [in-dir template-name cont meta]
-  (html5 (render-template-partially in-dir template-name cont meta)))
+  [template-name cont meta]
+  (html5 (render-template-partially template-name cont meta)))
 
 (defn render-page
   "Render HTML file from markdown file."
-  [post in-dir & template]
+  [post & template]
   (let [file (:file post)
         template (or (first template) "post")
-        [prev next] (pager (all-posts in-dir) post)]
-    (render-template in-dir
-                     template
-                     {:content (split-and-to-html in-dir file)}
-                     {:site-name (:site-name (config in-dir))
-                      :site-url (:site-url (config in-dir))
+        [prev next] (pager (all-posts) post)]
+    (render-template template
+                     {:content (split-and-to-html file)}
+                     {:site-name (:site-name (config @in-dir))
+                      :site-url (:site-url (config @in-dir))
                       :title (:title (file-metadata file))
                       :url   (page-url file)
                       :date (:date (file-metadata file))
@@ -179,46 +178,46 @@ the doctype."
 
 (defn generate-index
   "Generate content for index.html"
-  [in-dir]
+  []
   (println "Generating index...")
-  (render-template in-dir
-                   "index"
-                   (all-posts in-dir)
-                   {:site-name (:site-name (config in-dir))}))
+  (render-template "index"
+                   (all-posts)
+                   {:site-name (:site-name (config @in-dir))}))
 
-(defn write-index [in-dir output]
+(defn write-index [output]
   (spit (str output "/index.html")
-        (generate-index in-dir)))
+        (generate-index)))
 
 (defn prepare-dirs
   "Prepare directory structure."
-  [in-dir output]
+  [output]
   (println "Preparing directory structure...")
   (let [output-structure (reduce (fn [dirs file]
                                    (let [slug (page-url file)]
                                      (conj dirs (str output slug))))
-                                 #{(str output "/feeds")} (all-page-and-post-files in-dir))]
+                                 #{(str output "/feeds")}
+                                 (all-page-and-post-files @in-dir))]
     (doall (map fs/mkdirs output-structure))))
 
 (defn write-pages
   "Write HTML files to location."
-  [in-dir output]
+  [output]
   (println "Writing posts and pages...")
-  (doseq [post (concat (all-posts in-dir)
-                       (all-pages in-dir))]
+  (doseq [post (concat (all-posts)
+                       (all-pages))]
     (let [file (:file post)
           slug (page-url file)
           metadata (file-metadata file)]
       (spit (str output "/" slug "/index.html")
-            (render-page post in-dir (or (:template metadata)
-                                         nil))))))
+            (render-page post (or (:template metadata)
+                                  nil))))))
 
 (defn copy-resources
   "Copy in-dir/resources containing js,css and images"
-  [in-dir output]
+  [output]
   (println "Copying resources...")
   (do (fs/delete-dir (str output "/resources"))
-      (fs/copy-dir (str in-dir "/resources") (str output "/resources"))))
+      (fs/copy-dir (str @in-dir "/resources") (str output "/resources"))))
 
 ;; Feed
 (defn generate-feed
@@ -244,49 +243,50 @@ the doctype."
                       posts))
        (spit (str output "/feeds/" tag ".xml"))))
 
-(defn generate-main-feed [in-dir output]
+(defn generate-main-feed [output]
   (println "Generating main feed...")
-  (generate-feed (map :file (all-posts in-dir))
+  (generate-feed (map :file (all-posts))
                  "all"
-                 (config in-dir)
+                 (config @in-dir)
                  output))
 
-(defn generate-tag-feed [in-dir output tag tag-buckets]
+(defn generate-tag-feed [output tag tag-buckets]
   (generate-feed (doall (map :file (get tag-buckets tag)))
                  tag
-                 (config in-dir)
+                 (config @in-dir)
                  output))
 
 (defn load-custom-code
   "Load the custom code that can be placed in 'code/'."
-  [in-dir]
+  []
   (println "Loading custom code...")
   (doall
    (map (fn [file]
           (binding [*ns* (the-ns 'ecstatic.code)
-                    *in-dir* in-dir]
+                    *in-dir* @in-dir]
             (try (load-file (.getPath file))
                  (catch Exception e
                    (do (error "Could not load"
                               (fs/base-name file)
                               ":" (timbre/color-str :red e))
                        (System/exit 0))))))
-        (code-files in-dir))))
+        (code-files @in-dir))))
 
 (defn create-site
   "Read and create posts."
   [in-dir output]
-  (do (load-custom-code in-dir)
-      (prepare-dirs in-dir output)
-      (write-index in-dir output)
-      (write-pages in-dir output)
-      (generate-main-feed in-dir output)
+  (do (reset! ecstatic.core/in-dir in-dir)
+      (load-custom-code)
+      (prepare-dirs output)
+      (write-index output)
+      (write-pages output)
+      (generate-main-feed output)
       
-      (let [tag-buckets (tag-buckets in-dir)]
+      (let [tag-buckets (tag-buckets)]
         (doseq [tag (keys tag-buckets)]
-          (generate-tag-feed in-dir output tag tag-buckets)))
+          (generate-tag-feed output tag tag-buckets)))
       
-      (copy-resources in-dir output)
+      (copy-resources output)
       (println "Successfully compiled site.")))
 
 (defn auto-regen [in-dir output]
